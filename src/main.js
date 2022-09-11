e5c13638311d354438e424c1ad36ae4c9b5ddf9d
nitoa-s <kitamakeai2025@gmail.com>
2022-09-12 08:01:35 +0900

    環境構築&チュートリアル通りにやってみた

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  ipcMain.handle('ping', () => 'pong')
+  mainWindow.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
